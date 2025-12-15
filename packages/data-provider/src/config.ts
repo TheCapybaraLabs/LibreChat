8b346cdd315db36449dc39d83db484ed8ab88ebb
Fabrício <fab.brito94@gmail.com>
2025-12-15 14:11:09 -0300

    fix: remove duplicate entry in balance check

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -1134,7 +1134,6 @@ export const supportsBalanceCheck = {
   [EModelEndpoint.azureAssistants]: true,
   [EModelEndpoint.azureOpenAI]: true,
   [EModelEndpoint.bedrock]: true,
-  [EModelEndpoint.google]: true,
 };
 
 export const visionModels = [
