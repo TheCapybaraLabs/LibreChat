a6b952a8ec401e2fdeda40c0b6eaba9903c03bb5
Fabrício brito <fab.brito94@gmail.com>
2026-04-12 00:01:52 -0300

    fix: disable LaTeX parsing by default

--- client/src/store/settings.ts
+++ client/src/store/settings.ts
@@ -42,7 +42,7 @@ const localStorageAtoms = {
 
   // Beta features settings
   modularChat: atomWithLocalStorage('modularChat', true),
-  LaTeXParsing: atomWithLocalStorage('LaTeXParsing', true),
+  LaTeXParsing: atomWithLocalStorage('LaTeXParsing', false),
   centerFormOnLanding: atomWithLocalStorage('centerFormOnLanding', true),
   showFooter: atomWithLocalStorage('showFooter', true),
 
